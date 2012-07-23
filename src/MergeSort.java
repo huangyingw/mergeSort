79b8079c496e340a87d0cefd68f8c0e7b800371f
huangyingw@linux_programming <huangyingw@gmail.com>
2012-07-23 09:23:28 -0500

    n

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -29,6 +29,37 @@ class MergeSort {
 		}
 	}
 
+	public void mergeEffecient(int low, int mid, int high) {
+		int l, i, r;
+		int b[] = new int[50];
+		l = low;
+		i = low;
+		r = mid + 1;
+
+		while ((l <= mid) && (r <= high)) {
+			if (data[l] <= data[r]) {
+				b[i++] = data[l];
+				l++;
+			} else {
+				b[i++] = data[r];
+				r++;
+			}
+		}
+		if (l > mid) {
+			for (int k = r; k <= high; k++) {
+				b[i] = data[k];
+				i++;
+			}
+		} else {
+			for (int k = l; k <= mid; k++) {
+				b[i] = data[k];
+				i++;
+			}
+		}
+		for (int k = low; k <= high; k++)
+			data[k] = b[k];
+	}
+
 	public void mergeSort() {
 		// TODO Auto-generated method stub
 		mergeSort(0, data.length - 1);
@@ -39,7 +70,7 @@ class MergeSort {
 			int pivot = (left + right) / 2;
 			mergeSort(left, pivot);
 			mergeSort(pivot + 1, right);
-			merge(left, pivot, right);
+			mergeEffecient(left, pivot, right);
 			System.out.print("Now mergeSort," + left + "," + right + "->");
 			Print(left, right);
 
